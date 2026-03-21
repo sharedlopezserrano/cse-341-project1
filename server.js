aa526b0328596bc49b5539c982e7560c11a8dd77
sharedlopezserrano <slopezserrano@byupathway.edu>
2026-03-21 17:01:55 -0300

    Render #2

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const mongodb = require('./data/database');
-const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,7 +10,6 @@ app.use(express.json());
 app.use('/', require('./routes/index'));
 app.use('/contacts', require('./routes/contacts'));
 app.use('/api-docs', require('./routes/swagger'));
-app.use(cors());
 
 mongodb.initDb((err) => {
   if (err) {
